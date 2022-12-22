8ffb2d67b6f1c06576a1fbf1028c23ef257222a9
benjamin kisenge <benkisenge03@gmail.com>
2022-12-22 16:32:06 +0200

    Update test

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
+    before :each do
       get '/foods/index'
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/foods/show'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe 'GET /show' do
+  #   it 'returns http success' do
+  #     get '/foods/show'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
