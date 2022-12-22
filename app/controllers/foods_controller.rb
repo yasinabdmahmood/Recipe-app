131c2b4d0557b559fabd2471e37b60d2646b64e8
benjamin kisenge <benkisenge03@gmail.com>
2022-12-22 10:39:17 +0200

    Create the table of foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,31 @@
 class FoodsController < ApplicationController
   def index
+    @foods = current_user.foods
   end
 
-  def show
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Recipe.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      redirect_to food_index_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    Food.find_by(id: params[:id]).destroy
+    redirect_to food_index_path
+  end
+
+  private
+
+  def food_params
+    params.require(:new_recipe).permit(:name, :meadurement_unit, :price, :quantity)
   end
 end
