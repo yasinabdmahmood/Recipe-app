0869a12019e43c3bbd5780c46178a750fa0e7c11
benjamin kisenge <benkisenge03@gmail.com>
2022-12-23 11:43:55 +0200

    Run test for the recipe_food model

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.new(name: 'benkis')
+    @food = Food.new(user: @user, name: 'food_name', meadurement_unit: 'kg',price: 5, quantity: 10)
+    @recipe = Recipe.new(user: @user, name: 'test name', preparation_time: 1,
+      cooking_time: 1, description: 'test description', public: true)
+    @recipe_food = RecipeFood.new(food: @food, recipe: @recipe, quantity: 10)
+  end
+
+  it 'name must not be blank' do
+    @recipe_food.quantity = nil
+    expect(subject).to_not be_valid
+  end
 end
